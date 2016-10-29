96f8ef788e12f618cc48fee770cc7a478045b29d
chandrakanth <chandra619kanth@gmail.com>
2016-10-29 16:39:37 -0700

    init

---
+++ server/config/environment/development.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+    // MongoDB connection options
+    mongo: {
+        //uri: 'mongodb://localhost/resume-dev'
+        uri: 'mongodb://Chandra619kanth:Mcmacha#1@ds041486.mlab.com:41486/resume'
+    },
+
+    seedDB: true
+};
